3fb8895d9148380065f692f321d841975546992e
Jaden Brescia <jbresci1@villanova.edu>
2024-09-15 19:49:08 -0400

    Finished HW

---
+++ Homework_Two/Homework_Two/Assets/hw2.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hw2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] float xValue = 1;
+    [SerializeField] float yValue = 0;
+    [SerializeField] float zValue = 0;
+    [SerializeField] float moveSpeed = 10f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+    }
+}
+
